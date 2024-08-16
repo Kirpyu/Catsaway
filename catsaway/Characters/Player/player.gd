af2c535390a2f6bc0ef659ae53508ce32b15425d
Kyle Li <likyle701@gmail.com>
2024-08-17 04:32:02 +0800

    new stuff

---
+++ catsaway/Characters/Player/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+var direction :Vector2 = Vector2.ZERO
+@export var movespeed := 50
+
+func _physics_process(_delta):
+	direction = Input.get_vector("left","right","up","down").normalized()
+	set_velocity(direction * movespeed)
+	move_and_slide()
+	direction = velocity
