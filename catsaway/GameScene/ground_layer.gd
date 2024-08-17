8045adb065f0866d5bb6860cffc99026457e0e22
Kyle Li <likyle701@gmail.com>
2024-08-17 15:48:11 +0800

    cleaning up mess

--- catsaway/GameScene/ground_layer.gd
+++ catsaway/GameScene/ground_layer.gd
@@ -4,7 +4,8 @@ var tile : Vector2i
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("LMB"):
-		set_cell(tile, 0, Vector2i(4,1), 0)
+		set_cells_terrain_connect([tile], 0, 0)
+
 
 func _process(_delta: float) -> void:
 	tile = local_to_map(get_global_mouse_position())
